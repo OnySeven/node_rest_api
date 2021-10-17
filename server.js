d51a7343ed2d6ff9483aa0ad653abec1b6d9a0b0
ONY SEVEN <onysaptanugraha@gmail.com>
2021-10-17 23:25:25 +0700

    settin routes di server js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const app = express();
 
+//panggil routes 
+
+var routes = require('./routes');
+routes(app);
+
 //parse appliation/json
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
